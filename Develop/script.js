7abd25827124e0ca442ca411af02c7dfedf02d80
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-01-08 12:23:21 -0600

    generate lowercase password

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,6 @@ var passwordInfo = {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
   passwordInfo.length = window.prompt(
@@ -18,11 +17,11 @@ function writePassword() {
   );
   passwordInfo.length = parseInt(passwordInfo.length);
   switch (true) {
-      case isNaN(passwordInfo.length):
-      case passwordInfo.length < 8:
-      case passwordInfo.length > 128:
-        writePassword();
-  };
+    case isNaN(passwordInfo.length):
+    case passwordInfo.length < 8:
+    case passwordInfo.length > 128:
+      writePassword();
+  }
   function characterTypes() {
     passwordInfo.lowercase = window.confirm(
       "🐱‍💻 Rad. Would you like lowercase characters included in your password?"
@@ -36,7 +35,7 @@ function writePassword() {
     passwordInfo.special = window.confirm(
       "🐱‍💻 Almost done. Wanna throw some special characters into the mix?"
     );
-  };
+  }
 
   // Ensure at least one character type is selected
   characterTypes();
@@ -44,35 +43,44 @@ function writePassword() {
     console.log(passwordInfo);
     var passwordArray = Object.values(passwordInfo);
     passwordArray.shift();
-    console.log(passwordArray)
+    console.log(passwordArray);
     var allFalse = true;
     for (let i = 0; i < passwordArray.length; i++) {
       if (passwordArray[i] === true) {
         allFalse = false;
         break;
       }
-    };
+    }
     if (allFalse) {
-    window.alert("🐱‍💻 Wuh oh! You need to select at least one character type!");
+      window.alert(
+        "🐱‍💻 Wuh oh! You need to select at least one character type!"
+      );
       characterTypes();
       checkArray();
     }
-  };
+  }
   checkArray();
 
   window.alert("🐱‍💻 Alright, alright, alright! Enjoy your new password!");
 
   // Password Generator
   function generatePassword() {
-    return "Hello, World."
-  };
+    let str = "";
+    const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    const lowercaseLength = lowercaseCharacters.length;
+    for (let i = 0; i < passwordInfo.length; i++) {
+      str += lowercaseCharacters.charAt(
+        Math.floor(Math.random() * lowercaseLength)
+      );
+    }
+    return str;
+  }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
