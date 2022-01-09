eed3cb8b942f1165ea915749a77d751a571b5a6d
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-01-09 11:34:45 -0600

    generate password w/o bugs

--- Develop/script.js
+++ Develop/script.js
@@ -15,13 +15,19 @@ function writePassword() {
   passwordInfo.length = window.prompt(
     "🐱‍💻 Greetings! How long do you want your password to be? (Please enter a number between 8 and 128)"
   );
+
+  // Turn length value into a number
   passwordInfo.length = parseInt(passwordInfo.length);
+
+  // Ensure length value meets requirements
   switch (true) {
     case isNaN(passwordInfo.length):
     case passwordInfo.length < 8:
     case passwordInfo.length > 128:
       writePassword();
   }
+
+  // Collect prompt information from user while updating info object
   function characterTypes() {
     passwordInfo.lowercase = window.confirm(
       "🐱‍💻 Rad. Would you like lowercase characters included in your password?"
@@ -36,86 +42,157 @@ function writePassword() {
       "🐱‍💻 Almost done. Wanna throw some special characters into the mix?"
     );
   }
+  characterTypes();
 
   // Ensure at least one character type is selected
-  characterTypes();
   function checkArray() {
     console.log(passwordInfo);
+
+    // Convert object values into an array
     var passwordArray = Object.values(passwordInfo);
+
+    // Removes length value
     passwordArray.shift();
     console.log(passwordArray);
+
+    // Checks whether all prompts were false
     var allFalse = true;
     for (let i = 0; i < passwordArray.length; i++) {
+      // If even one value is true then move on
       if (passwordArray[i] === true) {
         allFalse = false;
         break;
       }
     }
+
+    // If all are still false, then...
     if (allFalse) {
+      // ...alert user...
       window.alert(
         "🐱‍💻 Wuh oh! You need to select at least one character type!"
       );
+
+      // ...re-run prompts...
       characterTypes();
+
+      // ...and re-run check
       checkArray();
     }
   }
+
+  // Original function call to check prompts
   checkArray();
 
+  // If prompts are successful alert user
   window.alert("🐱‍💻 Alright, alright, alright! Enjoy your new password!");
 
   // Password Generator
   function generatePassword() {
+    // Declare character types
     const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-    const lowercaseLength = lowercaseCharacters.length;
-
     const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const uppercaseLength = uppercaseCharacters.length;
-
     const numericCharacters = "0123456789";
-    const numericLength = numericCharacters.length;
-
     const specialCharacters = ` !"#$%&'()*+,-./:;<=>?@[\]^_\u0060{|}~`;
-    const specialLength = specialCharacters.length;
 
-    // An alternate list of special characters for stricter password systems
+    // An alternate list of special characters for stricter password systems,
+    // as is the case with Oracle Identity Manager and Microsoft Active Directory
     // const altSpecialCharacters = `@%+\/'!#$^?:,(){}[]~\u0060-_.`;
 
+    // Declare empty container to store final string
     var str = "";
 
-    var passwordArray = Object.values(passwordInfo);
-    passwordArray.shift();
-    var allTrue = true;
-    for (let i = 0; i < passwordArray.length; i++) {
-      if (passwordArray[i] === false) {
-        allTrue = false;
-        break;
-      }
+    // Declare empty container to store character choices
+    var choices = [];
+
+    // Check user choices and add selected characters to the array
+    if (passwordInfo.lowercase === true) {
+      choices += lowercaseCharacters;
+    }
+    if (passwordInfo.uppercase === true) {
+      choices += uppercaseCharacters;
+    }
+    if (passwordInfo.numeric === true) {
+      choices += numericCharacters;
+    }
+    if (passwordInfo.special === true) {
+      choices += specialCharacters;
     }
 
-    // debugger;
+    console.log(choices);
 
-    if (allTrue) {
-      for (let i = 0; i < passwordInfo.length / 4; i++) {
-        str += lowercaseCharacters.charAt(
-          Math.floor(Math.random() * lowercaseLength)
-        );
-      }
-      for (let i = 0; i < passwordInfo.length / 4; i++) {
-        str += uppercaseCharacters.charAt(
-          Math.floor(Math.random() * uppercaseLength)
-        );
-      }
-      for (let i = 0; i < passwordInfo.length / 4; i++) {
-        str += numericCharacters.charAt(
-          Math.floor(Math.random() * numericLength)
-        );
-      }
-      for (let i = 0; i < passwordInfo.length / 4; i++) {
-        str += specialCharacters.charAt(
-          Math.floor(Math.random() * specialLength)
-        );
+    // Add random character based on desired password length
+    function addCharacters() {
+      // Resets string to empty in case of re-running
+      str = "";
+
+      // Loops through password, adding random characters from selected choices
+      for (let i = 0; i < passwordInfo.length; i++) {
+        str += choices.charAt(Math.floor(Math.random() * choices.length));
       }
     }
+    addCharacters();
+
+    // Convert each string into an array for comparison
+    var lowArr = lowercaseCharacters.split("");
+    var upperArr = uppercaseCharacters.split("");
+    var numArr = numericCharacters.split("");
+    var specialArr = specialCharacters.split("");
+
+    // Check password to ensure it has array choices
+    function checkPassword(arr1, arr2) {
+      return arr1.some((characters) => arr2.includes(characters));
+    };
+
+    // Checks to ensure string includes all character types selected
+    function finalCheck() {
+
+      // Logs the string to be checked
+      console.log(str);
+
+      // converts string to be checked into an array for comparison
+      var strArr = str.split("");
+
+      // Checks string against earlier prompt choices, and...
+      if (passwordInfo.lowercase === true) {
+
+        // ...returns true if string doesn't contain any characters from specified array
+        if (!checkPassword(strArr, lowArr)) {
+          
+          // alerts developer
+          console.log("string did not include lowercase characters.");
+
+          // rebuilds a new string
+          addCharacters();
+
+          // and checks it again for posterity ;D
+          finalCheck();
+        }
+      }
+      if (passwordInfo.uppercase === true) {
+        if (!checkPassword(strArr, upperArr)) {
+          console.log("string did not include uppercase characters.");
+          addCharacters();
+          finalCheck();
+        }
+      }
+      if (passwordInfo.numeric === true) {
+        if (!checkPassword(strArr, numArr)) {
+          console.log("string did not include numeric characters.");
+          addCharacters();
+          finalCheck();
+        }
+      }
+      if (passwordInfo.special === true) {
+        if (!checkPassword(strArr, specialArr)) {
+          console.log("string did not include special characters.");
+          addCharacters();
+          finalCheck();
+        }
+      }
+    }
+    finalCheck();
+
+    // outputs final string value
     return str;
   }
   var password = generatePassword();
